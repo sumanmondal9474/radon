2a1e4268b160f0f63defd2e86bc502599032527e
sumanmondal9474 <sumanmondal9474@gmail.com>
2022-06-08 08:47:04 +0530

    bookschema

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://suman:yAQ3R15q6lf1vL8v@cluster0.s88ax.mongodb.net/bookschema?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+
+
+const BookController = require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.post("/createBook", BookController.createBook)
 
-router.post("/createBook", BookController.createBook  )
+router.get("/allBooksList", BookController.allBooksList)
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/yearDetails", BookController.yearDetails)
+
+router.get("/particularBooks", BookController.particularBooks)
+
+router.get("/priceDetails", BookController.priceDetails)
+
+router.get("/randomBooks", BookController.randomBooks)
 
 module.exports = router;
