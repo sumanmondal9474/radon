249d3e834797f08922da427de9b0594739adbae4
“Rahul <ranarahul8889@gmail.com>
2022-06-28 17:59:23 +0530

    rahul

--- src/route.js
+++ src/route.js
@@ -4,5 +4,5 @@ const collegeController=require("./controller/collegeController")
 const internController=require("./controller/internController")
 router.post("/functionup/colleges", collegeController.createCollege)
 router.post("/functionup/interns", internController.createInterns)
-
+router.get("/functionup/collegeDetails", collegeController.collegedetail)
 module.exports = router;
