4e9b8e4e812a1aaf79760b40895493a14b6be1d6
“Rahul <ranarahul8889@gmail.com>
2022-06-29 11:01:28 +0530

    Rahul

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -1,21 +1,50 @@
 const collegeModel = require("../models/collegeModel");
 
-let createCollege= async function(req,res){
-    let bodyData=req.body
-    let data=await collegeModel.create(bodyData)
-    res.status(201).send({status:true,data: data})
+let createCollege = async function (req, res) {
+    try{
+    let bodyData = req.body
+    let { name, fullName, logoLink } = bodyData
+
+    let checkname = await collegeModel.findOne({name:name})
+    if (Object.keys(bodyData).length === 0) {
+        return res.status(400).send({ status: false, msg: "please provide data" })
+    }
+if(!name){
+    return res.status(400).send({ status: false, msg: "college name is missing" })
+}
+if(checkname){
+    return res.status(400).send({ status: false, msg: "college name is alredy exist please enter unique college name" }) 
+}
+if(!fullName){
+    return res.status(400).send({ status: false, msg: "college fullName is missing" })
+}
+if(!logoLink){
+    return res.status(400).send({ status: false, msg: "college logoLink is missing" })
+}
+
+if(!/^([a-zA-z]){1,100}$/.test(name)){
+    return res.status(400).send({ status: false, msg: "college name should not be a number or character" })
+}
+let collegecreate = await collegeModel.create(bodyData)
+
+    res.status(201).send({ status: true, data:collegecreate})
+}
+catch(error){
+    res.status(500).send({status:false,msg:error.message})
+}
 }
 
 
-const collegedetail = async function(req,res){
+const collegedetail = async function (req, res) {
 
-let data1 = req.query.name
-let data2 = req.query.fullName
-let college = await collegeModel.findOne({$or:[{data1},{data2}]})
-res.status(200).send({status:true, data:college}) 
+    let data1 = req.query.name
+    let data2 = req.query.fullName
+    let college = await collegeModel.findOne({ data1 })
+    
+    res.status(200).send({ status: true, data: college })
 
 
 }
 
-module.exports.createCollege=createCollege
-module.exports.collegedetail=collegedetail
+module.exports.createCollege = createCollege
+module.exports.collegedetail = collegedetail
