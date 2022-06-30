df62230c1dace60cfb6e23859766f616e228c957
“Rahul <ranarahul8889@gmail.com>
2022-06-30 11:23:27 +0530

    rahul

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -1,9 +1,21 @@
 const collegeModel = require("../models/collegeModel");
 const internModel = require("../models/internModel");
+
+function validURL(myURL) {
+    let regex = (/^(https:\/\/|http:\/\/)[a-zA-Z!_$]+\-[a-zA-Z]+\.[a-zA-Z3]+\.[a-z]+\-[a-z]+\-[1]+\.[a-z]+\.[com]+\/[radon]+\/[a-zA-Z]+\.(jpeg|jpg|png|gif|webp)$/)
+    return regex.test(myURL)
+ }
+function trimall(alltrim){
+    return alltrim.trim()
+}
+
 let createCollege = async function (req, res) {
     try{
     let bodyData = req.body
     let { name, fullName, logoLink } = bodyData
+trimall(name);
+
+trimall(fullName);
 
     let checkname = await collegeModel.findOne({name:name})
     if (Object.keys(bodyData).length === 0) {
@@ -21,10 +33,12 @@ if(!fullName){
 if(!logoLink){
     return res.status(400).send({ status: false, msg: "college logoLink is missing" })
 }
-
-if(!/^([a-zA-z]){1,100}$/.test(name)){
-    return res.status(400).send({ status: false, msg: "college name should not be a number or character" })
+if(!validURL(logoLink)){
+    return res.status(400).send({status:false, msg:"url not valid"})
 }
+// if(!/^([a-zA-z]){1,100}$/.test(name)){
+//     return res.status(400).send({ status: false, msg: "college name should not be a number or character" })
+// }
 let collegecreate = await collegeModel.create(bodyData)
 
     res.status(201).send({ status: true, data:collegecreate})
@@ -45,7 +59,7 @@ let checkname = await collegeModel.find({name:data1})
 if(checkname.length==0){
     return res.status(400).send({status:false,msg:"please provide valid college name"})
 }
-let college = await collegeModel.findOne({data1 , isDeleted:false},{updatedAt:0,createdAt:0,isDeleted:0,__v:0}).lean()
+let college = await collegeModel.findOne({name:data1 , isDeleted:false},{updatedAt:0,createdAt:0,isDeleted:0,__v:0}).lean()
 let collegeId=college._id
 let interns=await internModel.find({collegeId:collegeId},{_id:1,updatedAt:0,createdAt:0,isDeleted:0,__v:0,collegeId:0}).lean()
 college.interns=interns
@@ -57,3 +71,17 @@ res.status(200).send({data:college})
 
 module.exports.createCollege = createCollege
 module.exports.collegedetail = collegedetail
+
+// From FnUr001_AayushKumar_9304154450 to Everyone 07:31 PM
+// queries return an instance of the Mongoose Document class. Documents are much heavier than vanilla Javascript objects, because they have a lot of internal state for change tracking. Enable the lean option tells Mongoose to skip instantiating a full Mongoose document and just give you the plain old Javascript objects (POJOs)
+//              Lean document are more than 10x smaller than the mongoose document.
+
+//              The downside of enabling lean is that lean docs don't have :
+//               'Change tracking,
+//               'Casting and validation,
+//               'Getters and setters,
+//               'Virtuals
+//               'save()
+
+//               When to use Lean : If we're executing a query and sending the results without modification,to say, an Express response, you should use lean. In general, if you do not modify the query results and do not use custom getters, you should use lean(). If you modify the query results or rely on features like getters or transforms,we should not use lean().
+        
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -67,3 +67,8 @@ let createInterns = async function (req, res) {
 
 
 module.exports.createInterns = createInterns
+
+// const logoLinkValidator = (/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g).test(logoLink)
+//             if (!logoLinkValidator) {
+//                 return res.status(400).send({ status: false, message: "Please enter a valid logo link " })
+//             }
